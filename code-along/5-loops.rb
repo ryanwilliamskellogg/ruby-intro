162b809baaa30eaa65d8f22811a601fdb630e8ea
Ryan Williams <ryan.williams@kellogg.northwestern.edu>
2025-04-29 13:42:08 +0000

    loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,9 +3,30 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
-# loop do
-#   puts "tacos!"
-# end
+loop do
+  puts "tacos!"
+  if index = 0 
+    break
+    # Break gets you out of the loop / ends the loop
+end
+index = index + 1
+# if index does not equal 10 it adds one to the number
+end 
 
-# Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+Loop through tacos
+tacos = ["carnitas", "carne asada", "pollo", "pescado", "tofu"]
+index = 0
+
+loop do
+    if index = tacos.count
+        break
+    end
+    puts tacos[index]
+
+    index = index +1
+end
+
+for taco in tacos
+    taco = tacos[index]
+    puts taco
+end
